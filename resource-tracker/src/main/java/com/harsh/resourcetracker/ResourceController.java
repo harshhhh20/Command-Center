9707c4edb90394eb4a0ef27c69dfdae807d179ef
harshhhh20 <harsh20.exe@gmail.com>
2026-08-03 14:53:59 +0530

    fix: add missing HashMap import to ResourceController

--- resource-tracker/src/main/java/com/harsh/resourcetracker/ResourceController.java
+++ resource-tracker/src/main/java/com/harsh/resourcetracker/ResourceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/resources")
